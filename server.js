200a9f10777060f183642798b6b5c07e648001fd
Gianni <gianni.tricarico@heh.be>
2016-08-15 22:04:16 +0200

    Modification du texte

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 
 app.get('/',function(req,res){
-    res.send('Test docker');
+    res.send('Test docker version 2');
 });
 
 app.listen(3000);
